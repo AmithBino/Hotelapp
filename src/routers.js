1e3f6183982b64210c8d6515a0f156a94c5443c7
Amith Bino <amith1062@gmail.com>
2023-07-16 21:24:51 +0530

    first commit

---
+++ src/routers.js
@@ -0,0 +1,23 @@
+import HomePage from './components/Home.vue'
+import SignUp from './components/SignUp.vue'
+import {createRouter,createWebHashHistory} from 'vue-router'
+
+const routes=[
+    {
+        name:'HomePage',
+        component : HomePage,
+        path:'/'
+    },
+    {
+        name:'SignUp',
+        component: SignUp,
+        path:'/sign-up'
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(), //hash mode for vue js routing
+    routes
+})
+
+export default router;
